88dbeb3914af3575cb536597f57595a6300af96b
Gregory Andrews <Gregory.Andrews@outlook.com>
2014-10-26 09:30:08 -0400

    2nd version of MyFirstApp
    2nd updated version of MyFirstApp

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var session=null;
 $(document).ready(function(){
     var loadCastInterval=setInterval(function(){
         if (chrome.cast.isAvailable) {
@@ -8,4 +9,30 @@ $(document).ready(function(){
             console.log('Unavailable');
         }
     },1000);
+    function initializeCastApi() {
+        var applicationID=new chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID;
+        var sessionRequest=new chrome.cast.SessionRequest(applicationID);
+        var ApiConfig=new chrome.cast.ApiConfig(sessionRequest,sessionListener, receiverListener);
+        chrome.cast.initialize(apiConfig,onInitSuccess,onInitError);
+    };
+    function sessionListener(e) {
+        session=e;
+        console.log('New session');
+        if (session.media.length != 0) {
+            console.log('Found' + session.media.length + 'sessions.');
+        }
+    }
+    function receiverListener(e) {
+        if (e === 'available') {
+            console.log("Chromecast was found on the network.");
+        } else{
+            console.log("There are no Chromecasts available.");
+        }
+    }
+    function onInitSuccess(){
+        console.log("Initialization succeeded.");
+    }
+    function onInitError(){
+        console.log("Initialization failed.")
+    }
 })
